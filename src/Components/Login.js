991b7155550de6e697d0edc36e440f4ea909a2ad
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-05-27 16:13:41 -0400

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,69 +1,115 @@
-import React from 'react'
-import styled from 'styled-components'
-import photo from './Mingling.jpg'
-import { Link } from 'react-router-dom'
-
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import photo from "./Mingling.jpg";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = (props) => {
+  const [formState, setFormState] = useState([
+    {
+      username: "",
+      password: "",
+    },
+  ]);
 
-        const { userName, password, submit, change } = props
+  //   const [errorState, setErrorState] = useState([
+  //     {
+  //       username: "",
+  //       password: "",
+  //     },
+  //   ]);
 
-        const onSubmit = evt => {
-            evt.preventDefault()
-            submit()
-        }
+  const { userName, password, submit, change } = props;
 
-        const onChange = evt => {
-            const { userName , password } = evt.target
-            change( userName, password )
-        }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
 
-    return (
+    axios
+      .post(`https://potluck-planner-4-backend-2.herokuapp.com/`, formState)
+      .then((res) => {
+        console.log("logging in", res.data);
+      });
+    setFormState(formState);
+  };
 
-        <LoginDiv>
-            <LoginImg src= { photo } alt = "People attending an event" />
-            <InfoDiv>
-                <h1>Enter your Username and Password.</h1>
-                <form id = "login-form" onSubmit = {onSubmit}>
-                    <label>
-                        <h3>Username: </h3>
-                        <input id = "userName" name = "userName" type = "text" onChange = {onChange} value = {userName} />
-                    </label>
-                    <label>
-                        <h3>Password: </h3>
-                        <input id = "password" name = "password" type = "text" onChange = {onChange} value = {password} />
-                    </label>
-                    <Link to = {`event-display`}>
-                        <button id = "login-button">Submit</button>
-                    </Link>
-                </form>
-            </InfoDiv>
-        </LoginDiv>
-    )
-}
+  const validate = (e) => {
+    const userInput = {
+      ...formState,
+      [e.target.name]: e.target.value,
+    };
+  };
+
+  const onChange = (evt) => {
+    const { userName, password } = evt.target.value;
+    // change(userName, password);
+    validate(evt);
+    console.log("changing");
+
+    const setValue = evt.target.value;
+    setFormState({ ...formState, [evt.target.name]: setValue });
+  };
+
+  return (
+    <>
+      <LoginDiv>
+        <LoginImg src={photo} alt="People attending an event" />
+        <InfoDiv>
+          <h1>Enter your Username and Password.</h1>
+          <form id="login-form" onSubmit={onSubmit}>
+            <label htmlFor="username">
+              <h3>Username: </h3>
+              <input
+                id="userName"
+                name="username"
+                type="text"
+                onChange={onChange}
+                value={formState.value}
+                //   value={userName}
+              />
+            </label>
+            <label htmlFor="password">
+              <h3>Password: </h3>
+              <input
+                id="password"
+                name="password"
+                type="text"
+                onChange={onChange}
+                value={formState.value}
+                //   value={password}
+              />
+            </label>
+            <Link to={`event-display`}>
+              <button id="login-button">Submit</button>
+            </Link>
+          </form>
+        </InfoDiv>
+      </LoginDiv>
+    </>
+  );
+};
 
 const LoginDiv = styled.div`
-display: flex;
-color: white;
-background: black;
-flex-direction: row;
-justify-content: space-between;
-align-items: start;
-padding: 5%;
-height: 100vh;
-`
+  display: flex;
+  color: white;
+  background: black;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: start;
+  padding: 5%;
+  height: 100vh;
+`;
 
 const LoginImg = styled.img`
-width: 50%;
-`
+  width: 50%;
+`;
 
 const InfoDiv = styled.div`
-display: flex;
-width: 50%;
-flex-direction: column;
-Justify-content: left;
-align-items: center;
-`
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+  justify-content: left;
+  align-items: center;
+`;
 
-export default Login
+export default Login;
